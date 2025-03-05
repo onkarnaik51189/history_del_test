8fed3dcaad3bc88f8cfc0b90b8f357b3dad3d6b5
Onkar Naik <onkar.naik@nxo.com>
2025-03-05 11:21:46 +0530

    function added for addition

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+
+int add(int num1, int num2) {
+	return num1+num2;
+}
+
 int main(){
-	int a,b,c;
+	int a,b;
 	printf("Addition App\n");
 	printf("Enter 2 numbers\n");
 	scanf("%d %d", &a, &b);
-	c=a+b;
-	printf("Sum is %d\n",c);
+	printf("Sum is %d\n",add(a,b));
 }
